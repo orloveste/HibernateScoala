bbdd719469d9186b09c43169073019a14d0ae3b8
orloveste <orlovevm@yahoo.com>
2020-02-05 15:57:16 +0200

    create NoteService class -getNota -createNota -updateNota -deleteNota

---
+++ src/main/java/NoteService.java
@@ -0,0 +1,69 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class NoteService {
+    private static NoteService instance;
+
+    public NoteService(){
+
+    }
+
+    public static NoteService getInstance() {
+        if (instance == null){
+            instance = new NoteService();
+        }
+        return instance;
+    }
+
+    public Note getNota(Note id){
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            return session.find(Note.class, id);
+        }catch (Exception exGetNota){
+            exGetNota.printStackTrace();
+            return null;
+        }
+    }
+
+    public  void createNota(Note nota){
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(nota);
+            transaction.commit();
+        }catch (Exception exCreateNota){
+            if (transaction != null){
+                transaction.rollback();
+            }exCreateNota.printStackTrace();
+        }
+    }
+    public void updateNota (Note nota){
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(nota);
+            transaction.commit();
+        }catch (Exception exUpdateNota){
+            if (transaction!=null){
+                transaction.rollback();
+            }exUpdateNota.printStackTrace();
+        }
+    }
+
+
+    public void deleteNota(Note nota){
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.delete(nota);
+            transaction.commit();
+        }catch (Exception exDeleteNota){
+            if (transaction != null){
+                transaction.rollback();
+            }exDeleteNota.printStackTrace();
+        }
+    }
+}
